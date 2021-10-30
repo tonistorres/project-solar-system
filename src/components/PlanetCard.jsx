7a855445e91df6ecef1832c2e929bc83888837eb
tonistorres <sisvenda2011@gmail.com>
2021-10-29 23:10:53 -0300

    feat:Qt-05 Respondida -Componente PlanetCard Implementado

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <h2 data-testid="planet-name">{planetName}</h2>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+export default PlanetCard;
