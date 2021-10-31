b6da987c15ace22347574497f163e70482497d81
tonistorres <sisvenda2011@gmail.com>
2021-10-31 18:48:24 -0300

    feat:Qt-08 Respondida Renderizando Title em Missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
-    return <div data-testid="missions" />;
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
+    );
   }
 }
 
